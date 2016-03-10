91c81f1d2b480d1a6b92875b75c9e2e5876ac5c2
Troy Cornwall <git@troycornwall.net>
2016-03-10 17:35:22 +1300

    new ui

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,61 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var usersOnline = 0;
-  var totalUsers = 0;
+
   var SlackClient = require('slack-api-client');
+  var async = require('async');
+  var ago = require('ago');
   var slack = new SlackClient(process.env.SLACK_TOKEN);
-  slack.api.users.list({
-    presence: 1
-  }, function (err, users) {
-    if (err) { throw err; }
-    var members = users.members;
-    for(var i = 0; i < members.length; i++){
-      var user = members[i];
-      if(user.deleted  === false && user.is_bot === false ){
-        totalUsers += 1;
-        if( user.presence !== 'away') {
-          usersOnline += 1;
+
+  async.parallel([
+        function (callback) {
+          slack.api.users.list({
+            presence: 1
+          }, function (err, users) {
+            if (err) {
+              throw err;
+            }
+            var members = users.members;
+            var onlineUsers = 0;
+            var totalUsers = 0;
+            for (var i = 0; i < members.length; i++) {
+              var user = members[i];
+              if (user.deleted === false && user.is_bot === false) {
+                totalUsers += 1;
+                if (user.presence !== 'away') {
+                  onlineUsers += 1;
+                }
+              }
+            }
+            callback(null, {onlineUsers: onlineUsers, totalUsers: totalUsers});
+          });
+        },
+        function (callback) {
+          var ts = (ago(3, "h")+'').slice(0,9);
+          slack.api.channels.history({
+              channel: 'C03DB1NV5'
+            }, function(err, history){
+              var numMessages = 0;
+              var messages = history.messages;
+              for(var i = 0; i < messages.length; i++){
+                var messagets = messages[i].ts.slice(0,9);
+                if(messagets < ts){
+                  numMessages += 1;
+                }
+              }
+              callback(null, {messagesLast3h: numMessages});
+          });
         }
-      }
-    }
-    //console.log(usersOnline + "/" + totalUsers);
-    res.render('index', { title: 'Tech Offtopic - Slack Invite', numOnline: usersOnline, totalUsers: totalUsers });
-  });
+      ],
+      function (err, results) {
+        res.render('index', {
+          title: 'Tech Offtopic - Slack Invite',
+          onlineUsers: results[0].onlineUsers,
+          totalUsers: results[0].totalUsers,
+          messagesLast3h: results[1].messagesLast3h
+        });
+      });
 });
+
 
 module.exports = router;
